4cacbf394e758d8d8e6fbd1986d725ecd64775c0
Francisco Soto <franciscosotobw@gmail.com>
2026-02-06 21:46:34 -0300

    Add updateProduct route to products.routes.js

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -5,17 +5,20 @@ import {
   createProduct,
   deleteProduct,
   listMyProducts,
+  updateProduct,
 } from "../controllers/products.controller.js";
 import { requireAuth } from "../middleware/auth.middleware.js";
 
 const router = Router();
 
+// públicas
 router.get("/", listProducts);
-router.get("/mine", requireAuth, listMyProducts);
 router.get("/:id", getProduct);
 
 // protegidas
+router.get("/mine", requireAuth, listMyProducts);
 router.post("/", requireAuth, createProduct);
+router.put("/:id", requireAuth, updateProduct);
 router.delete("/:id", requireAuth, deleteProduct);
 
 export default router;
